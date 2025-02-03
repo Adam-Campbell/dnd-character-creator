c838fbfb4386971a1c9ecdbeb7293df905713d37
Adam Campbell <adamcampbelldev@gmail.com>
2025-02-03 13:53:25 +0000

    add profile header and profile bio editing

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.templatetags.static import static
+
+def get_default_image_url():
+    return static('images/profile-image-placeholder.webp')
 
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
-    profile_image_url = models.URLField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True, default="")
+    profile_image_url = models.URLField(default=get_default_image_url)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from characters.models import Character
+from .forms import BioForm
 
 # Create your views here.
 
@@ -12,3 +15,21 @@ def profile(request, user_id):
 
     is_own_profile = (user == request.user)
     return render(request, 'userprofile/profile.html', {  'user': user, 'created_characters': created_characters, 'is_own_profile': is_own_profile })
+
+
+@login_required
+def edit_profile_bio(request, user_id):
+    if request.method == 'POST':
+        # grab the user and related user profile]
+        user = User.objects.select_related('userprofile').get(id=user_id)
+        # if the user is not the current user, return a 403
+        if user != request.user:
+            return HttpResponseForbidden("You are not allowed to edit this user's profile")
+        form = BioForm(request.POST, instance=user.userprofile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user_id=user_id)
+
+@login_required
+def upload_profile_image(request, user_id):
+    pass
