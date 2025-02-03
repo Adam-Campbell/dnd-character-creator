12a98e0883d7de6f3ad270e6e83b45d0db726aec
Adam Campbell <adamcampbelldev@gmail.com>
2025-02-03 01:08:13 +0000

    begin implementing user profile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('<int:user_id>', views.profile, name='profile')
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from characters.models import Character
 
 # Create your views here.
 
-def profile(request):
-    return render(request, 'userprofile/profile.html')
+def profile(request, user_id):
+    # get the user and related user profile
+    user = User.objects.select_related('userprofile').get(id=user_id)
+    # grab the created characters for the user
+    created_characters = Character.objects.filter(user=user)
+
+    is_own_profile = (user == request.user)
+    return render(request, 'userprofile/profile.html', {  'user': user, 'created_characters': created_characters, 'is_own_profile': is_own_profile })
