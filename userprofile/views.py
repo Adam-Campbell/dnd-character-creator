db323765112d16c62530c610fdc210a14ed7e99f
Adam Campbell <adamcampbelldev@gmail.com>
2025-02-03 15:27:21 +0000

    complete user profile view

--- userprofile/views.py
+++ userprofile/views.py
@@ -4,17 +4,34 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from characters.models import Character
 from .forms import BioForm
+from characters.data_utils import get_static_data, get_item_by_id
 
 # Create your views here.
 
 def profile(request, user_id):
     # get the user and related user profile
     user = User.objects.select_related('userprofile').get(id=user_id)
+    is_own_profile = (user == request.user)
+    # grab static character data for enriching the characters
+    static_character_data = get_static_data()
     # grab the created characters for the user
     created_characters = Character.objects.filter(user=user)
+    for character in created_characters:
+        character.class_data = get_item_by_id(static_character_data['classes'], str(character.character_class))
+        character.race_data = get_item_by_id(static_character_data['races'], str(character.race))
+    # grab the liked characters for the user
+    liked_characters = user.liked_characters.all()
+    for character in liked_characters:
+        character.class_data = get_item_by_id(static_character_data['classes'], str(character.character_class))
+        character.race_data = get_item_by_id(static_character_data['races'], str(character.race))
 
-    is_own_profile = (user == request.user)
-    return render(request, 'userprofile/profile.html', {  'user': user, 'created_characters': created_characters, 'is_own_profile': is_own_profile })
+    
+    return render(request, 'userprofile/profile.html', {  
+        'user': user, 
+        'created_characters': created_characters,
+        'liked_characters': liked_characters,
+        'is_own_profile': is_own_profile 
+    })
 
 
 @login_required
